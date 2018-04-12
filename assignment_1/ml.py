3b7b6ba5aba1d813287031792176e113f2a28d71
guanchen zhu <zhuguanchen@me.com>
2018-04-13 00:53:42 +0200

    preprocess. but how to fill Na?

--- assignment_1/ml.py
+++ assignment_1/ml.py
@@ -7,7 +7,24 @@ class ML:
         pass
 
     def preprocess(self, df):
-        pass
+		df['time'] = df['time'].astype(str).str[:-4]
+		df['time'] = pd.to_datetime(df['time'], errors='coerce')
+		df.drop(['Unnamed: 0', 'Unnamed: 0.1'], axis=1, inplace=True)
+		
+		#mood needs to be averaged
+		df_mood = df.loc[df['variable'] == 'mood']
+		df_mood = df_mood.groupby(['id', 'time'])['value'].mean().to_frame(name = 'mood_mean').reset_index()
+
+		#others need to be summed
+		grouped = df.groupby(['id','variable', 'time'])['value'].sum()
+		grouped = grouped.unstack(level=[1]).reset_index()
+		grouped.dropna(how='all', inplace=True)
+		grouped = pd.concat([grouped, df_mood])
+		grouped.drop('mood', axis=1, inplace=True)
+
+		#NA to be filled with mean of sum
+		'''still subject to change'''
+		grouped.fillna(grouped.mean(), inplace=True)
 
     def prediction(self, df):
         pass
